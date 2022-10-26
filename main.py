452a5ce173df53042c816585050c102599124846
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 00:52:19 +0000

    .count method seems to work

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 # Make your Code
 # ******************************
 strval = input().split()
-numbers = []
-for v in strval:
-	numbers.append(int(v))
-print (numbers)
-# the below llin 11 are same as the lines from 5 to 8
-# numbers = list(map(int, strval))
-# print (numbers)
+numbers = list(map(int, strval))
+#print (numbers)
+
+keyword = int(input("one number: "))
+
+countme = numbers.count(keyword)
+print(countme)
